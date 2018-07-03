e91af65955dd9d29e48af0c4d9e89c8da89c0ecc
taijiang01 <taijiang01@gmail.com>
2018-07-04 00:25:30 +0800

    403.blade.php

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -2,4 +2,9 @@
 @section('content')
 <h1>403 Forbidden.</h1>
 <p>您沒有權限可以執行目前的動作喔！</p>
+<?php
+    $default_error_message = "快滾回 <a href='".url('')."'>首頁</a> 吧！";
+  ?>
+  {!! isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message
+  !!}
 @endsection
