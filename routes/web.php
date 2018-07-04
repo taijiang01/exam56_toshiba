b158df2efadf364ea704bdf1da0803981f3c6152
taijiang01 <taijiang01@gmail.com>
2018-07-04 21:55:57 +0800

    將資料寫進資料庫

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/exam', 'ExamController@index')->name('exam.index');
 Route::get('/exam/create', 'ExamController@create')->name('exam.create');
+Route::post('/exam', 'ExamController@store')->name('exam.store');
+
 
